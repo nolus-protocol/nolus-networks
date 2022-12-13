493894e999069d2274c3004d214b0cbd2ac0f7f0
d.ivanov <d.ivanov@nolus.io>
2022-12-13 17:48:02 +0200

    Add dockerfile for nolus nodes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # nolus-networks
+
+## Build && Run node with docker
+
+Executing the build-image.sh script will build an image for testnet/nolus-rila network.
+In the future when we have more testnets, any user could specify his desired network via --network flag passed to the build-image.sh script
+
+1. Execute build-image.sh script to build && run a full node of testnet/nolus-rila with Docker
+
+```./build-image.sh```
---
+++ build-image.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+NETWORK="testnet/nolus-rila"
+
+__print_usage() {
+    printf \
+    "Usage:
+    [--network <network_name>]"
+}
+
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+
+  -h | --help)
+    __print_usage "$0"
+    exit 0
+    ;;
+
+  -c | --network)
+    NETWORK="$2"
+    shift
+    shift
+    ;;
+
+  *)
+    echo "unknown option '$key'"
+    exit 1
+    ;;
+
+  esac
+done
+
+VERSION=$(cat "$NETWORK"/version.md)
+IMAGE_NAME="nolus-image-$VERSION"
+
+docker build \
+  --build-arg NETWORK="$NETWORK" \
+  --build-arg VERSION="$VERSION" \
+  -f nolus_node.Dockerfile \
+  -t "$IMAGE_NAME" .
+
+docker run -d -it \
+  --name "nolus-node-$VERSION" \
+  -v nolusDataVol:/.nolus/data \
+  "$IMAGE_NAME"
